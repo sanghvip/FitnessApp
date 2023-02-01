ed4861994c18f203eb93ec53b5d6ea4f14bd76c2
Pratik Sanghvi <pratiksanghvi.2016@gmail.com>
2023-02-01 22:50:56 +0000

    Commit material module for ngmaterial packages

---
+++ fitness-tracker/src/app/material.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from "@angular/core";
+
+/** 
+ * Module file to manage the import from Angular material
+ * */ 
+@NgModule({
+    imports:[],
+    exports:[]
+})
+export class MaterialModule {
+
+}
