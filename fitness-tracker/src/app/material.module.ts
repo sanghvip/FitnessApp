5eae5066944bbf1495935879f4838fd63f165cc1
sanghvip <pratiksanghvi1994@gmail.com>
2023-02-04 09:31:37 -0500

    Updating signup component

--- fitness-tracker/src/app/material.module.ts
+++ fitness-tracker/src/app/material.module.ts
@@ -1,13 +1,14 @@
 import { NgModule } from "@angular/core";
 import {MatButtonModule} from '@angular/material/button';
 import { MatIcon, MatIconModule } from "@angular/material/icon";
-
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
 /** 
  * Module file to manage the import from Angular material
  * */ 
 @NgModule({
-    imports:[MatButtonModule,MatIconModule],
-    exports:[MatButtonModule,MatIconModule]
+    imports:[MatButtonModule,MatIconModule, MatFormFieldModule,MatInputModule],
+    exports:[MatButtonModule,MatIconModule,MatFormFieldModule,MatInputModule]
 })
 export class MaterialModule {
 
