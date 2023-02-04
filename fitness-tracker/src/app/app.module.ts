cd44dca89e22b44f875d7c52f45b99a6e93e2f98
sanghvip <pratiksanghvi1994@gmail.com>
2023-02-03 19:50:06 -0500

    Creating auth and new training components

--- fitness-tracker/src/app/app.module.ts
+++ fitness-tracker/src/app/app.module.ts
@@ -5,10 +5,20 @@ import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
 import { MaterialModule } from './material.module';
+import { SignupComponent } from './auth/signup/signup.component';
+import { LoginComponent } from './auth/login/login.component';
+import { TrainingComponent } from './training/training.component';
+import { CurrentTrainingComponent } from './training/current-training/current-training.component';
+import { NewTrainingComponent } from './training/new-training/new-training.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SignupComponent,
+    LoginComponent,
+    TrainingComponent,
+    CurrentTrainingComponent,
+    NewTrainingComponent
   ],
   imports: [
     BrowserModule,
--- fitness-tracker/src/app/material.module.ts
+++ fitness-tracker/src/app/material.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from "@angular/core";
+import {MatButtonModule} from '@angular/material/button';
+import { MatIcon, MatIconModule } from "@angular/material/icon";
 
 /** 
  * Module file to manage the import from Angular material
  * */ 
 @NgModule({
-    imports:[],
-    exports:[]
+    imports:[MatButtonModule,MatIconModule],
+    exports:[MatButtonModule,MatIconModule]
 })
 export class MaterialModule {
 
