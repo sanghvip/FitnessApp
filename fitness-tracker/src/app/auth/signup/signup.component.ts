40802dea23fa91b3ac0e8ecd23e3a12ce506fc3f
sanghvip <pratiksanghvi1994@gmail.com>
2023-02-05 19:05:36 -0500

    Solving moment package error

--- fitness-tracker/src/app/auth/signup/signup.component.ts
+++ fitness-tracker/src/app/auth/signup/signup.component.ts
@@ -1,12 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import moment from "moment";
+import { Moment } from 'moment';
 
 @Component({
   selector: 'app-signup',
   templateUrl: './signup.component.html',
   styleUrls: ['./signup.component.css']
 })
-export class SignupComponent {
+export class SignupComponent implements OnInit{
+  maxDate: Moment;
+
+  constructor(){
+    this.maxDate = moment().subtract(18,'year')
+    // this.maxDate.setFullYear(this.maxDate.getFullYear() - 18);
+  }
+
+  ngOnInit()
+  {    
+  }
+  
 
   onSubmit(form: NgForm){
 console.log(form);
