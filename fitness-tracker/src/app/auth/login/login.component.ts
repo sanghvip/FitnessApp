c378ad650a2452844080a7a8be0efc72f19f483f
sanghvip <pratiksanghvi1994@gmail.com>
2023-02-22 16:18:30 -0500

    Adding login component for the app

--- fitness-tracker/src/app/auth/login/login.component.ts
+++ fitness-tracker/src/app/auth/login/login.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit{
+  
+  ngOnInit(){
+
+  }
+
+  onSubmit(form: NgForm){
+    console.log(form);
+      }
+
 
 }
