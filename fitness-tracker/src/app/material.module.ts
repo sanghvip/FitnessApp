30f9c1d5f23d80b6358055c14bff1436a03b824c
sanghvip <pratiksanghvi1994@gmail.com>
2023-02-23 18:36:07 -0500

    Adding general font and navigation links.

--- fitness-tracker/src/app/material.module.ts
+++ fitness-tracker/src/app/material.module.ts
@@ -6,24 +6,30 @@ import { MatInputModule } from '@angular/material/input';
 import { MatDatepickerModule } from '@angular/material/datepicker';
 import { MatNativeDateModule } from "@angular/material/core";
 import { MatCheckboxModule } from "@angular/material/checkbox";
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatToolbarModule} from '@angular/material/toolbar';
 /** 
  * Module file to manage the import from Angular material
  * */
 @NgModule({
     imports: [MatButtonModule,
-         MatIconModule,
-         MatFormFieldModule,
-         MatInputModule,
-         MatDatepickerModule,
-         MatNativeDateModule,
-        MatCheckboxModule],
+        MatIconModule,
+        MatFormFieldModule,
+        MatInputModule,
+        MatDatepickerModule,
+        MatNativeDateModule,
+        MatCheckboxModule,
+        MatSidenavModule,
+    MatToolbarModule],
     exports: [MatButtonModule,
-         MatIconModule,
-         MatFormFieldModule,
-         MatInputModule,
-         MatDatepickerModule,
-         MatNativeDateModule,
-        MatCheckboxModule]
+        MatIconModule,
+        MatFormFieldModule,
+        MatInputModule,
+        MatDatepickerModule,
+        MatNativeDateModule,
+        MatCheckboxModule,
+        MatSidenavModule,
+        MatToolbarModule]
 })
 export class MaterialModule {
 
