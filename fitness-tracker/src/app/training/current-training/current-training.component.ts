943ad4a9f62a5a85ddd2752df93f4a8c59531fc6
sanghvip <pratiksanghvi1994@gmail.com>
2023-02-26 16:13:46 -0500

    dynamic progress value updates

--- fitness-tracker/src/app/training/current-training/current-training.component.ts
+++ fitness-tracker/src/app/training/current-training/current-training.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-current-training',
   templateUrl: './current-training.component.html',
   styleUrls: ['./current-training.component.css']
 })
-export class CurrentTrainingComponent {
+export class CurrentTrainingComponent implements OnInit {
+  progress = 0;
+
+  ngOnInit(){
+
+    setInterval(()=>{
+      this.progress = this.progress+5;
+    },1000);
+  }
 
 }
