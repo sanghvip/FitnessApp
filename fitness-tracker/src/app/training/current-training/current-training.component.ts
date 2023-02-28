81bcdbbf24019240c42878e7ebd6659711b081f6
sanghvip <pratiksanghvi1994@gmail.com>
2023-02-27 20:09:55 -0500

    Adding stop timer functionality

--- fitness-tracker/src/app/training/current-training/current-training.component.ts
+++ fitness-tracker/src/app/training/current-training/current-training.component.ts
@@ -12,10 +12,13 @@ export class CurrentTrainingComponent implements OnInit {
     this.timer = setInterval(()=>{
       this.progress = this.progress+5;
       if(this.progress>=100){
-        this.progress=0;
         clearInterval(this.timer);
       }
     },1000);
+  }
+
+  onStop(){
+    clearInterval(this.timer);
   }
 
 }
