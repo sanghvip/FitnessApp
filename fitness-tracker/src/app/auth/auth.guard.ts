8866ca127c91543786ee91af61e1c945f39f8299
sanghvip <pratiksanghvi1994@gmail.com>
2023-03-02 14:32:37 -0500

    Add basic route guard.

---
+++ fitness-tracker/src/app/auth/auth.guard.ts
@@ -0,0 +1,17 @@
+import {ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot} from '@angular/router';
+import {Injectable} from '@angular/core';
+import { AuthService } from './auth.service';
+
+@Injectable()
+export class AuthGurad implements CanActivate{
+
+    constructor(private authService: AuthService){
+
+    }
+
+    canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot)
+    {
+        return this.authService.isAuth();
+    }
+
+}
