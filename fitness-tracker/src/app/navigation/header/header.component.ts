0def0130755696e414bc0b272ed5e4ea79ac469e
sanghvip <pratiksanghvi1994@gmail.com>
2023-03-01 20:27:48 -0500

    Subscribe and unsubscribe from authService.

--- fitness-tracker/src/app/navigation/header/header.component.ts
+++ fitness-tracker/src/app/navigation/header/header.component.ts
@@ -1,12 +1,27 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component, EventEmitter, Output, OnInit, OnDestroy } from '@angular/core';
+import { AuthService } from 'src/app/auth/auth.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
+  constructor(private authService:AuthService){}
   @Output() sidenavToggle = new EventEmitter<void>();
+  isAuth: boolean;
+  authSubscription: Subscription;
+
+  ngOnInit(){
+    this.authSubscription = this.authService.authChange.subscribe(authStaus => {
+      this.isAuth = authStaus;
+    });
+  }
+
+  ngOnDestroy(){
+    this.authSubscription.unsubscribe();
+  }
 
   onToggleSidenav() {
     this.sidenavToggle.emit();
