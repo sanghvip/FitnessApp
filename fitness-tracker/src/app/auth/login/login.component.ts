140ace36e58a7366a5d02662f9a9dfd7c19bfca7
sanghvip <pratiksanghvi1994@gmail.com>
2023-03-01 19:37:14 -0500

    Adding auth service for login.

--- fitness-tracker/src/app/auth/login/login.component.ts
+++ fitness-tracker/src/app/auth/login/login.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import { AuthService } from '../auth.service';
 
 @Component({
   selector: 'app-login',
@@ -7,13 +8,17 @@ import { NgForm } from '@angular/forms';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit{
+
+  constructor(private authService:AuthService){
+
+  }
   
   ngOnInit(){
 
   }
 
   onSubmit(form: NgForm){
-    console.log(form);
+    this.authService.login();
       }
 
 
