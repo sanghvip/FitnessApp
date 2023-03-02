0101bb28eb08663b265831a5cffba1f4777c2bc4
sanghvip <pratiksanghvi1994@gmail.com>
2023-03-02 13:40:19 -0500

    Hooking Auth service in the sidenav component

--- fitness-tracker/src/app/navigation/sidenav-list/sidenav-list.component.ts
+++ fitness-tracker/src/app/navigation/sidenav-list/sidenav-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, EventEmitter, Output } from '@angular/core';
+import { AuthService } from 'src/app/auth/auth.service';
 
 @Component({
   selector: 'app-sidenav-list',
@@ -7,10 +8,17 @@ import { Component, EventEmitter, Output } from '@angular/core';
 })
 export class SidenavListComponent {
 @Output() closeSidenav = new EventEmitter<void>();
+isAuth = false;
+
+constructor(private authService:AuthService){
+
+}
 
   onClose(){
     this.closeSidenav.emit();
-
+    this.authService.authChange.subscribe(authStatus => {
+      this.isAuth = authStatus;
+    });
   }
 
 }
