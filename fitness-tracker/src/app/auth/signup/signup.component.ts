ce0d79533382e15f9686838087b4713070837265
sanghvip <pratiksanghvi1994@gmail.com>
2023-03-01 19:30:19 -0500

    Register auth service

--- fitness-tracker/src/app/auth/signup/signup.component.ts
+++ fitness-tracker/src/app/auth/signup/signup.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import { AuthService } from '../auth.service';
 // import moment from "moment";
 // import { Moment } from 'moment';
 
@@ -11,7 +12,7 @@ import { NgForm } from '@angular/forms';
 export class SignupComponent implements OnInit{
   minDate: Date;
 
-  constructor(){
+  constructor(private authService: AuthService){
     this.minDate = new Date();
     this.minDate.setFullYear(this.minDate.getFullYear() - 18);
   }
