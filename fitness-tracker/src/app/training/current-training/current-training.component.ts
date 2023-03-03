4fc495f0967787de02f5886a3aa0d0da710ba5d4
sanghvip <pratiksanghvi1994@gmail.com>
2023-03-03 10:40:55 -0500

    Hooking up cancel and complete training services.

--- fitness-tracker/src/app/training/current-training/current-training.component.ts
+++ fitness-tracker/src/app/training/current-training/current-training.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, EventEmitter, Output } from '@angular/core';
+import { Component, OnInit} from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { TrainingService } from '../training.service';
 import { StopTrainingComponent } from './stop-training.component';
@@ -9,7 +9,7 @@ import { StopTrainingComponent } from './stop-training.component';
   styleUrls: ['./current-training.component.css']
 })
 export class CurrentTrainingComponent implements OnInit {
-  @Output() trainingExit = new EventEmitter();
+  
   progress = 0;
   timer=0;
   constructor(private dialog:MatDialog, private trainingService: TrainingService){
@@ -25,7 +25,7 @@ export class CurrentTrainingComponent implements OnInit {
     const dialogRef = this.dialog.open(StopTrainingComponent, {data:{progress: this.progress}});
     dialogRef.afterClosed().subscribe(result => {
       if(result){
-        this.trainingExit.emit();
+        this.trainingService.cancelExercise(this.progress);
       }
       else{
         this.startOrResumeTimer();
@@ -38,6 +38,7 @@ export class CurrentTrainingComponent implements OnInit {
     this.timer = setInterval(()=>{
       this.progress = this.progress+5;
       if(this.progress>=100){
+        this.trainingService.completeExercise();
         clearInterval(this.timer);
       } 
     },step);
--- fitness-tracker/src/app/training/training.service.ts
+++ fitness-tracker/src/app/training/training.service.ts
@@ -2,7 +2,7 @@ import { Exercise } from "./exercise.model";
 import  {Subject} from "rxjs";
 
 export class TrainingService {
-    exerciseChanged = new Subject<Exercise>();
+    exerciseChanged = new Subject<any>();
     private availableExercises: Exercise[] = [
         {id:'crunches', name:'Crunches',duration:30,calories:8},
         {id:'touch-toes', name:'Touch Toes',duration:180,calories:1},
@@ -10,7 +10,8 @@ export class TrainingService {
         {id:'burpees', name:'Burpees',duration:120,calories:8} 
     ];
 
-    private runningExercise: Exercise;
+    private runningExercise: any;
+    private exercises: Exercise[] = [];
 
     getAvailableExercises(){
         return this.availableExercises.slice();
@@ -19,6 +20,22 @@ export class TrainingService {
     startExercise(selectedId: string){
         this.runningExercise = this.availableExercises.find(ex => ex.id===selectedId)!;
         this.exerciseChanged.next({...this.runningExercise});
+    }
+
+    completeExercise(){
+        this.exercises.push({ ...this.runningExercise, date: new Date(), state: 'completed' });
+        this.runningExercise = null;
+        this.exerciseChanged.next(null);
+    }
+
+    cancelExercise(progress: number){
+        this.exercises.push({ ...this.runningExercise,
+             date: new Date(), 
+             calories: this.runningExercise.duration * (progress/100),
+             duration: this.runningExercise.duration * (progress/100),
+             state: 'cancelled' });
+        this.runningExercise = null;
+        this.exerciseChanged.next(null);
     }
 
     getRunningExercise(){
