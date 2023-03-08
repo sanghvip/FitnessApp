42dff4298f28008a8d8090f395217d55ca489c04
sanghvip <pratiksanghvi1994@gmail.com>
2023-03-08 12:32:55 -0500

    connecting training service in new training comp

--- fitness-tracker/src/app/training/new-training/new-training.component.ts
+++ fitness-tracker/src/app/training/new-training/new-training.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { AngularFirestore } from '@angular/fire/compat/firestore';
-import { Observable } from 'rxjs';
+import { Observable, Subscription } from 'rxjs';
 import { map } from 'rxjs/operators';
 
 import { TrainingService } from '../training.service';
@@ -14,14 +14,16 @@ import { Exercise } from '../exercise.model';
 })
 export class NewTrainingComponent implements OnInit {
   exercises: Observable<Exercise[]>;
-
+  
   constructor(
     private trainingService: TrainingService,
     private db: AngularFirestore
   ) {}
 
   ngOnInit() {
-     this.exercises = 
+    this.trainingService.exercisesChanged.subscribe();
+    this.trainingService.fetchAvailableExercises();
+    //  this.exercises = 
       // this.exercises.subscribe(result => console.log("Data from firebase:"+result));
       // console.log("Exercise data:"+this.exercises);
   }
