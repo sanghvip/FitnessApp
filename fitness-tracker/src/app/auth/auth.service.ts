6f2e436fcfea3c8c81c9d544dc5198520f13a2f2
sanghvip <pratiksanghvi1994@gmail.com>
2023-03-09 21:53:32 -0500

    resolving promise from user creation.

--- fitness-tracker/src/app/auth/auth.service.ts
+++ fitness-tracker/src/app/auth/auth.service.ts
@@ -14,8 +14,15 @@ export class AuthService {
   constructor(private router: Router, private auth:AngularFireAuth) {}
 
   registerUser(authData: AuthData) {
-    this.auth.createUserWithEmailAndPassword(authData.email,authData.password);
-    this.authSuccessfully();
+    this.auth.createUserWithEmailAndPassword(authData.email,authData.password)
+    .then( result => {
+      console.log(result);
+      this.authSuccessfully();
+    })
+    .catch(error => {
+      console.log(error);
+    })
+    
   }
 
   login(authData: AuthData) {
