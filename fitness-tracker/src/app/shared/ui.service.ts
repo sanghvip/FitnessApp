8a1f96fbd50bc2bed87222df125b70edafc8de6e
sanghvip <pratiksanghvi1994@gmail.com>
2023-03-12 23:10:11 -0400

    adding new emthos to uiservice.

--- fitness-tracker/src/app/shared/ui.service.ts
+++ fitness-tracker/src/app/shared/ui.service.ts
@@ -1,6 +1,12 @@
+import { Injectable } from '@angular/core';
 import {Subject} from 'rxjs';
 
+@Injectable
 export class UIService {
     loadingChanged = new Subject<boolean>();
+
+    showSnackBar(message,action,duration){
+
+    }
 
 }
