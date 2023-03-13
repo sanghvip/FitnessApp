60476dad2b55247585e293b6ac09c8d560c9f8d3
sanghvip <pratiksanghvi1994@gmail.com>
2023-03-12 21:42:08 -0400

    Adding spinner to training component.

--- fitness-tracker/src/app/auth/signup/signup.component.ts
+++ fitness-tracker/src/app/auth/signup/signup.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { NgForm } from '@angular/forms';
-
+import { UIService } from 'src/app/shared/ui.service';
+import {Subscription} from 'rxjs';
 import { AuthService } from '../auth.service';
 
 @Component({
@@ -8,12 +9,18 @@ import { AuthService } from '../auth.service';
   templateUrl: './signup.component.html',
   styleUrls: ['./signup.component.css']
 })
-export class SignupComponent implements OnInit {
+export class SignupComponent implements OnInit, OnDestroy{
   maxDate: any;
+  isLoading = false;
+  private loadingSubs: Subscription;
 
-  constructor(private authService: AuthService) { }
+  constructor(private authService: AuthService, private uiService:UIService) { }
 
   ngOnInit() {
+    this.loadingSubs = this.uiService.loadingChanged.subscribe(isLoading => {
+      this.isLoading = isLoading;
+
+    });
     this.maxDate = new Date();
     this.maxDate.setFullYear(this.maxDate.getFullYear() - 18);
   }
@@ -24,5 +31,7 @@ export class SignupComponent implements OnInit {
       password: form.value.password
     });
   }
-
+ngOnDestroy(): void {
+  this.loadingSubs.unsubscribe();
+}
 }
--- fitness-tracker/src/app/training/new-training/new-training.component.ts
+++ fitness-tracker/src/app/training/new-training/new-training.component.ts
@@ -15,17 +15,17 @@ import { Exercise } from '../exercise.model';
 export class NewTrainingComponent implements OnInit, OnDestroy {
   exercises: Exercise[];
   exerciseSubscription: Subscription;
+  isLoading = true;
 
   constructor(
     private trainingService: TrainingService) {}
 
   ngOnInit() {
     this.exerciseSubscription = this.trainingService.exercisesChanged.subscribe(exercises => {
-      this.exercises = exercises});
+      this.exercises = exercises;
+      this.isLoading = false;
+    });
     this.trainingService.fetchAvailableExercises();
-    //  this.exercises = 
-      // this.exercises.subscribe(result => console.log("Data from firebase:"+result));
-      // console.log("Exercise data:"+this.exercises);
   }
 
   onStartTraining(form: NgForm) {
