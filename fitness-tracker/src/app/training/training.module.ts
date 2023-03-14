57831a1dfdf7ba9e8f5223df80624f9bc8358366
sanghvip <pratiksanghvi1994@gmail.com>
2023-03-14 12:51:37 -0400

    Adding code to the training module

--- fitness-tracker/src/app/training/training.module.ts
+++ fitness-tracker/src/app/training/training.module.ts
@@ -1,3 +1,18 @@
-@NgModule(declarations:[],
-    imports:[],entryComponents:[])
+import {NgModule} from '@angular/core';
+import { AngularFirestoreModule } from '@angular/fire/compat/firestore';
+import { PastTrainingComponent } from 'old_src/app/training/past-training/past-training.component';
+import { CurrentTrainingComponent } from './current-training/current-training.component';
+import { StopTrainingComponent } from './current-training/stop-training.component';
+import { NewTrainingComponent } from './new-training/new-training.component';
+import { TrainingComponent } from './training.component';
+
+@NgModule({
+declarations:[TrainingComponent,
+CurrentTrainingComponent,
+NewTrainingComponent,
+PastTrainingComponent,
+StopTrainingComponent],
+imports:[AngularFirestoreModule],
+entryComponents:[StopTrainingComponent]})
+
 export class TrainingModule{}
