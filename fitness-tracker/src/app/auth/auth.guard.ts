b91910a5ff2e230e354836e34c71bbacead78441
sanghvip <pratiksanghvi1994@gmail.com>
2023-03-15 11:42:21 -0400

    adding canload method

--- fitness-tracker/src/app/auth/auth.guard.ts
+++ fitness-tracker/src/app/auth/auth.guard.ts
@@ -2,16 +2,27 @@ import { Injectable } from '@angular/core';
 import {
   ActivatedRouteSnapshot,
   RouterStateSnapshot,
-  Router
+  Router,
+  CanActivate,
+  CanLoad
 } from '@angular/router';
 
 import { AuthService } from './auth.service';
 
 @Injectable()
-export class AuthGuard {
+export class AuthGuard implements CanActivate, CanLoad {
   constructor(private authService: AuthService, private router: Router) {}
 
   canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
+    if (this.authService.isAuth()) {
+      return true;
+    } else {
+      this.router.navigate(['/login']);
+      return false;
+    }
+  }
+
+  canLoad(route: ActivatedRouteSnapshot) {
     if (this.authService.isAuth()) {
       return true;
     } else {
