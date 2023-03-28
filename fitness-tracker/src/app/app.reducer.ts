7c05817166394b5aa7fcad64c136c5e8c05f216a
sanghvip <pratiksanghvi1994@gmail.com>
2023-03-27 20:57:49 -0400

    adding common reducer store

--- fitness-tracker/src/app/app.reducer.ts
+++ fitness-tracker/src/app/app.reducer.ts
@@ -1,22 +1,13 @@
-export interface State {
-    isLoading:boolean;
+import * as fromUi from './shared/ui.reducer';
+import { ActionReducerMap, createFeatureSelector, createSelector } from '@ngrx/store';
+
+export interface State{
+    ui: fromUi.State;
 }
 
-const initialState: State = {
-    isLoading: false
-};
-
-export function appReducer(state = initialState,action){
-    switch(action.type){
-        case 'START_LOADING':
-            return {
-                isLoading : true
-            }
-        case 'STOP_LOADING':
-            return {
-                isLoading: false
-            }
-        default:
-            return state;
-    }
+export const reducers: ActionReducerMap<State> = {
+    ui: fromUi.uiReducer
 }
+
+export const getUiState = createFeatureSelector<fromUi.State>('ui');
+export const getIsLoading = createSelector(getUiState, fromUi.getIsLoading);
